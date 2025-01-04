f9562f444092bbfb9f973211daf046588a87f1c0
whitewizardd <leumasre@gmail.com>
2025-01-04 16:20:57 +0100

    construction loaded

--- src/Bank.sol
+++ src/Bank.sol
@@ -7,9 +7,25 @@ pragma solidity ^0.8.26;
 import { ERC20 } from "solmate/tokens/ERC20.sol";
 // import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
 
+/**
+
+ * @title GameBank
+ * @dev A simple ERC20 token representing a game bank.
+ * @dev this is intended to be deployed upon every new creation of a new game.
+
+ */
 contract GameBank is ERC20("GameBank", "GB", 4) {
 
+    // the tolerance is the extra token minted to cater for player borrowing and community card picked .
     uint256 constant private tolerace = 4;
+
+
+    /**
+     * @dev Initializes the contract with a fixed supply of tokens.
+     * @param numberOfPlayers the total number of players.
+     * @dev _mint an internal function that mints the total token needed for the game.
+     */
+     
     constructor(uint8 numberOfPlayers) {
         uint256 amountToMint = numberOfPlayers + tolerace; 
         _mint(address(this), amountToMint);
