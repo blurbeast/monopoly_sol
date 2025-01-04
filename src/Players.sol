045bfed3ba992e5980e550090bebe33020e4d6c3
whitewizardd <leumasre@gmail.com>
2025-01-04 17:18:10 +0100

    create player

---
+++ src/Players.sol
@@ -0,0 +1,23 @@
+
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+
+contract PlayerS {
+
+    constructor () {
+
+    }
+
+    function registerPlayer() external {}
+
+    function joinGame(uint256 gameId) external {}
+
+    function createGame() external {}
+
+    function buyProperty(uint256 propertyId) external {}
+
+    function rentProperty(uint256 propertyId) external {}
+
+}
