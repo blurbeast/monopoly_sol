25e4573e17ffcdb4132bffe7a8b2d9e634efa04c
whitewizardd <leumasre@gmail.com>
2025-01-05 01:14:53 +0100

    added a private function that would convert all letters to alphabets

--- src/NFT.sol
+++ src/NFT.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.26;
 import "lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract GeneralNFT is ERC721URIStorage {
-
     uint8 public constant MAX_SUPPLY = 30;
     uint8 public totalSupply;
 
@@ -12,15 +11,13 @@ contract GeneralNFT is ERC721URIStorage {
     string private baseUri;
     mapping(address => uint256) private minterTokenId;
 
-    constructor(string memory name, string memory symbol, string memory desc, string memory uri)
-    ERC721(name, symbol)
-    {
+    constructor(string memory name, string memory symbol, string memory desc, string memory uri) ERC721(name, symbol) {
         decription = desc;
         baseUri = uri;
     }
 
     function mint(address _minter) external {
-        require(totalSupply < MAX_SUPPLY , "");
+        require(totalSupply < MAX_SUPPLY, "");
         uint256 tokenId = totalSupply + 1;
         totalSupply++;
         _mint(_minter, tokenId);
--- src/Players.sol
+++ src/Players.sol
@@ -1,16 +1,10 @@
-
-
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-
 contract PlayerS {
+    constructor() {}
 
-    constructor () {
-
-    }
-
-    function registerPlayer() external {}
+    function registerPlayer(address playerAddress, string memory username) external {}
 
     function joinGame(uint256 gameId) external {}
 
@@ -20,4 +14,17 @@ contract PlayerS {
 
     function rentProperty(uint256 propertyId) external {}
 
+    function convertToLowerCase(string memory username) private pure returns (string memory) {
+        bytes memory recievedUsernameBytes = bytes(username);
+        bytes memory convertedUsernameBytes = new bytes(recievedUsernameBytes.length);
+
+        for (uint256 i = 0; i < recievedUsernameBytes.length; i++) {
+            if ((uint8(recievedUsernameBytes[i]) >= 65) && (uint8(recievedUsernameBytes[i]) <= 90)) {
+                convertedUsernameBytes[i] = bytes1(uint8(recievedUsernameBytes[i]) + 32);
+            } else {
+                convertedUsernameBytes[i] = recievedUsernameBytes[i];
+            }
+            return string(convertedUsernameBytes);
+        }
+    }
 }
