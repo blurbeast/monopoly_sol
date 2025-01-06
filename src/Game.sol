1cc845aae9f4abf2f22ff7db885a0337b252a668
whitewizardd <leumasre@gmail.com>
2025-01-06 03:27:17 +0100

    game contract

--- src/Game.sol
+++ src/Game.sol
@@ -8,6 +8,15 @@ pragma solidity ^0.8.26;
 contract Game {
 
     address public gameBank;
+
+
+    /**
+        @dev this function initializes the game with the provided players.
+        @dev this function calls on the bank contract and mint a certain amount of tokens for each player.
+        @param players the addresses of the players.
+
+        @dev this function should emit an event
+     */
     function startGame(address[] memory players) external {
         
     }
