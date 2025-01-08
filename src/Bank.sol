5e80ce95748a02a10d622370879eff9805336d5a
whitewizardd <leumasre@gmail.com>
2025-01-08 23:23:33 +0100

    properties arranged into game contract

--- src/Bank.sol
+++ src/Bank.sol
@@ -4,13 +4,12 @@ pragma solidity ^0.8.26;
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 // import { ERC20 } from "lib/solmate/src/tokens/ERC20.sol";
 
-  struct Property {
-        bytes name;
-        uint256 rentAmount;
-        bytes uri;
-        uint256 buyAmount;
-    }
-
+struct Property {
+    bytes name;
+    uint256 rentAmount;
+    bytes uri;
+    uint256 buyAmount;
+}
 
 interface NFTContract {
     function getAllProperties() external view returns (Property[] memory);
@@ -23,8 +22,8 @@ interface NFTContract {
 
 contract GameBank is ERC20("GameBank", "GB") {
     struct PropertyG {
-        string name;
-        string uri;
+        bytes name;
+        bytes uri;
         uint256 buyAmount;
         uint256 rentAmount;
         address owner;
@@ -34,28 +33,25 @@ contract GameBank is ERC20("GameBank", "GB") {
     uint256 private constant tolerace = 4;
     NFTContract private nftContract;
     PropertyG[] gamePropertiesG;
-
     /**
      * @dev Initializes the contract with a fixed supply of tokens.
      * @param numberOfPlayers the total number of players.
      * @dev _mint an internal function that mints the total token needed for the game.
      */
+
     constructor(uint8 numberOfPlayers, address _nftContract) {
         uint256 amountToMint = numberOfPlayers + tolerace;
+        require(_nftContract.code.length > 0, "not a contract address");
         nftContract = NFTContract(_nftContract);
         _mint(address(this), amountToMint);
     }
 
     function gameProperties() private {
         uint256 size = nftContract.getAllProperties().length;
-        for(uint8 i = 1; i <= size; i++) {
+        for (uint8 i = 1; i <= size; i++) {
             Property memory property = nftContract.getAllProperties()[i];
-            gamePropertiesG[i] = PropertyG (
-                string(property.name),
-                string(property.uri),
-                property.buyAmount,
-                property.rentAmount,
-                address(this)
+            gamePropertiesG[i] = PropertyG(
+                property.name, property.uri, property.buyAmount, property.rentAmount, address(this)
             );
         }
     }
