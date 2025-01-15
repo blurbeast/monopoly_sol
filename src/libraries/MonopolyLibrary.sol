59ceb537ac50363b294f7a1511670b12faa1ea72
Ajidokwu Sabo <realjaiboi70@gmail.com>
2025-01-16 00:00:49 +0100

    chore: propose swap, accept deal, reject deal and counter deal

--- src/libraries/MonopolyLibrary.sol
+++ src/libraries/MonopolyLibrary.sol
@@ -46,6 +46,7 @@ library MonopolyLibrary {
         bool inJail;
         uint8 jailAttemptCount;
         uint256 cash;
+        uint8 diceRolled;
     }
     struct PropertyRent {
         uint8 propertyId;
@@ -57,4 +58,76 @@ library MonopolyLibrary {
         uint withHotel;
         uint costOfHouse;
     }
+
+    enum BenefitType {
+        NONE,
+        FREE_RENT,
+        RENT_DISCOUNT,
+        PROPERTY_ROYALTY
+    }
+
+    struct Benefit {
+        BenefitType benefitType;
+        bool isActive;
+        uint8 benefitValue;
+        uint8 numberOfTurns;
+    }
+
+    struct Proposal {
+        address user;
+        // address biddedUser;
+        uint8 proposedPropertyId;
+        uint8 biddedPropertyId;
+        uint256 biddedTokenAmount;
+        mapping(uint8 => Benefit) benefits;
+        uint8 numberOfBenefits;
+    }
+
+    struct Bid {
+        address bidder;
+        uint256 bidAmount;
+    }
+
+    struct PropertySwap {
+        address bidder;
+        uint8 biddersProperty;
+        uint8 yourProperty;
+        SWAP_TYPE swapType;
+        uint biddingAmount;
+    }
+
+    enum SWAP_TYPE {
+        PROPERTY_FOR_PROPERTY,
+        PROPERTY_FOR_CASH_AND_PROPERTY,
+        PROPERTY_AND_CASH_FOR_PROPERTY
+    }
+
+    event PropertySwapProposed(
+        address indexed proposer,
+        address indexed proposee,
+        uint8 biddingPropertyId,
+        uint8 propertyToSwapId,
+        SWAP_TYPE swapType
+    );
+
+    // Event to log the swap details
+    event DealAccepted(
+        address indexed user,
+        address indexed bidder,
+        uint8 biddersProperty,
+        uint8 yourProperty,
+        MonopolyLibrary.SWAP_TYPE swapType,
+        uint256 biddingAmount
+    );
+
+    // Event for counter deal tracking
+    event CounterDealProposed(
+        address indexed user,
+        uint8 biddingPropertyId,
+        uint8 propertyToSwapId,
+        MonopolyLibrary.SWAP_TYPE swapType,
+        uint256 biddingAmount
+    );
+
+    event DealRejected(address indexed user);
 }
