664045719cef088c37ad31f0a1d7e103a71f4a03
blurbeast <leumasre@gmail.com>
2025-03-06 23:24:58 +0100

    refactored the imports

--- src/account_abstraction/EntryPoint.sol
+++ src/account_abstraction/EntryPoint.sol
@@ -1,9 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "lib/account-abstraction/contracts/interfaces/IAccount.sol";
-import "lib/account-abstraction/contracts/interfaces/IPaymaster.sol";
+import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
+import {IPaymaster} from "lib/account-abstraction/contracts/interfaces/IPaymaster.sol";
 import {ISmartAccount} from "./interfaces/ISmartAccount.sol";
+import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
 
 contract EntryPoint {
     function handleOp(PackedUserOperation calldata userOp, bytes32 userOpHash) external returns (bytes memory) {
