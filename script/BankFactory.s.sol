e48f38701e1edc4c451ec7cf8b5016dec9d4aa42
blurbeast <leumasre@gmail.com>
2025-03-28 21:36:17 +0100

    bank factory

--- script/BankFactory.s.sol
+++ script/BankFactory.s.sol
@@ -15,10 +15,10 @@ contract BankFactoryScript is Script {
 
         // call on the bank factory contract using the new keyword
         //tells it to create an instance or object of the bank factory contract
-        BankFactory factory = new BankFactory(address(0));
+        BankFactory factory = new BankFactory(0x4A30f459F694876A5c6b726995274076dcD21E67);
 
         console.log("address of the factory contract", address(factory));
 
         vm.stopBroadcast();
     }
-}
+} // 0xe6f91F1986177a9BB54Bbcb37021422b08EeF3bE
