bc4982aa1c283490327f672706a3d1ac5d4fab8e
blurbeast <leumasre@gmail.com>
2025-03-28 01:19:41 +0100

    approve and allowance function done

--- src/GameToken.sol
+++ src/GameToken.sol
@@ -5,6 +5,8 @@ import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol"
 contract GameToken {
     // since this contract will be generic for all games , we'd create it in such a way to allow user balance at a particular game be used
     mapping(address => mapping(address => uint256 )) public playerBalance;
+    mapping(address => mapping(address => mapping(address => uint256))) public allowance;
+
     string public name;
     string public symbol;
     constructor() {
@@ -24,7 +26,7 @@ contract GameToken {
 
     function mintToPlayers(address[] memory players, uint256 amount, address contractAddress) external {
         for(uint8 i = 0; i < players.length; i++ ) {
-            this.transfer(contractAddress, players[i], amount);
+            this.transfer(contractAddress, contractAddress, players[i], amount);
         }
     }
 
@@ -37,11 +39,20 @@ contract GameToken {
             playerBalance[gameId][owner] -= amount;
             playerBalance[gameId][beneficiary] += amount;
         }else {
-
+            uint256 bal = this.balanceOf(owner, gameId);
+            require(bal >= amount, "insufficient balance");
+            playerBalance[gameId][owner] -= amount;
+            playerBalance[gameId][beneficiary] += amount;
         }
     }
 
     function balanceOf(address owner, address contractAddress) public returns(uint256) {
         return playerBalance[contractAddress][owner];
+    }
+
+    function approve(address gameId, address owner, address spender) external {
+        //we are approving the total uint256 max
+        // the approve is just adding a kinda layer
+        allowance[gameId][owner][spender] = type(uint256).max;
     }
 }
