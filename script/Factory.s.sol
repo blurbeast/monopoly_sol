1ee7940446a5d5947d896de694ba80a1c0309d21
blurbeast <leumasre@gmail.com>
2025-03-31 07:51:29 +0100

    updated

--- script/Factory.s.sol
+++ script/Factory.s.sol
@@ -6,10 +6,14 @@ import {Script, console} from "forge-std/Script.sol";
 
 contract BFactory is Script {
     function run() external {
-        //        BankFactory factory = BankFactory(0x29032371e9cd83185AD2D6dcc40E17370ba9A04E);
-        //        (uint256 id, address bank) =
-        //                            factory.deployGameBank(7, 0x98D8643215747e8B81e1b90424b644C3FCFf75ea);
-        //        console.log("id ", id);
-        //        console.log("address is ", bank);
+
+        uint256 pk = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(pk);
+                BankFactory factory = BankFactory(0xe6f91F1986177a9BB54Bbcb37021422b08EeF3bE);
+                address bank = factory.deployGameBank(7, 0x98D8643215747e8B81e1b90424b644C3FCFf75ea);
+//                console.log("id ", id);
+                console.log("address is ", bank);
+
+        vm.stopBroadcast();
     }
 }
--- script/GameBank.s.sol
+++ script/GameBank.s.sol
@@ -10,9 +10,18 @@ contract GameBankScript is Script {
     function setUp() external {}
 
     function run() external {
-        GameBank gameBank = GameBank(0xB5C1efD5e6C5c8F40D528FDd4aa31C943B41f1Cb);
-        address owner = gameBank.getPropertyOwner(2);
+//        GameBank gameBank = GameBank(0xB5C1efD5e6C5c8F40D528FDd4aa31C943B41f1Cb);
+//        address owner = gameBank.getPropertyOwner(2);
+//
+//        console.log("property is ::", owner);
+        uint256 pk = vm.envUint("PRIVATE_KEY");
 
-        console.log("property is ::", owner);
+        vm.startBroadcast(pk);
+        GameBank gameBank = new GameBank(7 , 0x98D8643215747e8B81e1b90424b644C3FCFf75ea, 0x4A30f459F694876A5c6b726995274076dcD21E67);
+
+        console.log("game bank ::: ", address (gameBank));
+
+        vm.stopBroadcast();
+
     }
 }
