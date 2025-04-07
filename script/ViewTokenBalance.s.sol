d0f6a67031d48c91b4a2fc916e9efe1c9f745036
blurbeast <leumasre@gmail.com>
2025-04-07 20:06:08 +0100

    loaded

--- script/ViewTokenBalance.s.sol
+++ script/ViewTokenBalance.s.sol
@@ -10,9 +10,9 @@ contract ViewBalance is Script {
 
     function run() external {
 
-        address contractAddress = address (0x3c992aC9922E08ACE021a793B068dE1Ca5e28896);
-        address player1SmartAccount = address (0x6235dCe6c3B06F54992F49E668b6227bf1814922);
-        address player2SmartAccount = address (0x612eD1c61b9278a7E5f3BC27168e47C8E39e04A7);
+        address contractAddress = address (0x9Bb0c295d374e45afc73ECEe231a8db0eA44be8F);
+        address player1SmartAccount = address (0xCE3bf0d25E319cC5DBb57729677E5D578C7ba47a);
+        address player2SmartAccount = address (0x5922E9FaF5Bba23dCb5E0d22a231239A99132034);
 
         GameToken token = GameToken(address (0x4A30f459F694876A5c6b726995274076dcD21E67));
         uint256 balance = token.balanceOf(player2SmartAccount, contractAddress);
